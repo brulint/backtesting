09e10fb6690db08a0815d857d6b86d8b87511962
brulint <102539902+brulint@users.noreply.github.com>
2023-04-07 04:03:09 +0200

    Update kissbacktest.py

--- kissbacktest.py
+++ kissbacktest.py
@@ -15,19 +15,17 @@ df['close'] = df.close.replace(to_replace=0, method='ffill')
 # df = pd.read_csv('btceur_4h.csv')[-1000:]
 
 # strategy
-RSI = ta.RSI(df.close,timeperiod=7)
-SIG_buy = RSI > 70
-SIG_sell = RSI < 30
+SIG_buy = # Signal d'achat
+SIG_sell = # Signal de vente
 
 # position
 SIG_0 = SIG_buy.astype(int) - SIG_sell.astype(int)
-SIG_1 = SIG_0.where(SIG_0 != 0).ffill()
-POS = SIG_1 > 0
+POS = SIG_0.where(SIG_0 != 0).ffill()
 
 # return
 r_0 = df.close / df.close.shift()
-r_strat = np.where(POS.shift(), r_0, 1)
-r_fee = np.where(POS.shift() + POS == 1, 1-0.0025, 1)
+r_strat = np.where(POS.shift() == 1, r_0, 1)
+r_fee = np.where(POS.shift() + POS == 0, 1-0.0025, 1)
 
 # cumulative return
 R_0 = np.nancumprod(r_0)
@@ -37,8 +35,8 @@ R_net = np.nancumprod(r_strat * r_fee)
 # graphic
 p1 = figure(height=300)
 p1.line(df.time, df.close)
-p1.triangle(df.time, df.close.where((POS == 1) & (POS.shift() == 0)), color = 'green', size = 7)
-p1.inverted_triangle(df.time,df.close.where((POS == 0) & (POS.shift() == 1)), color = 'red', size = 7)
+p1.triangle(df.time, df.close.where((POS == 1) & (POS.shift() == -1)), color = 'green', size = 7)
+p1.inverted_triangle(df.time,df.close.where((POS == -1) & (POS.shift() == 1)), color = 'red', size = 7)
 p2 =  figure(height=100)
 p2.line(df.time, RSI)
 p2.line(df.time, 70, color='green')

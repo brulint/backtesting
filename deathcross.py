40d0c50b26d3c0b1319035b5e73c07f911d6b90a
brulint <102539902+brulint@users.noreply.github.com>
2023-05-29 14:32:53 +0200

    Update deathcross.py

--- deathcross.py
+++ deathcross.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-import requests
 import numpy as np
 import pandas as pd
 import talib as ta
@@ -9,59 +8,59 @@ import talib as ta
 df = pd.read_csv('https://raw.githubusercontent.com/brulint/backtesting/main/btceur-4h.csv')
 
 # strategy
-EMA_20 = ta.EMA(df.close, timeperiod = 20)
-SMA_200 = ta.SMA(df.close, timeperiod = 200)
 
-position = EMA_20 > SMA_200
+df['fast'] = ta.EMA(df['close'], timeperiod = 20)
+df['slow'] = ta.SMA(df['close'], timeperiod = 200)
 
-position_in = df.close.where((position == 1) & (position.shift() == 0))
-position_out = df.close.where((position == 0) & (position.shift() == 1))
+df['position'] = df['fast'] > df['slow']
 
-# return
-r_0 = df.close / df.close.shift()
-r_strat = np.where(position.shift() == 1, r_0, 1)
-r_fee = np.where(position.shift() + position == 1, 1-0.0025, 1)
+# returns
 
-# cumulative return
-R_0 = np.nancumprod(r_0)
-R_strat = np.nancumprod(r_strat)
-R_net = np.nancumprod(r_strat * r_fee)
+df['r_hodl'] = df['close'] / df['close'].shift()
+df['r_strat'] = np.where(df['position'].shift() == 1, df['r_hodl'], 1)
+df['r_fee'] = np.where(df['position'] != df['position'].shift(), 1.0025, 1)
+df['r_net'] = df['r_strat'] / df['r_fee']
 
-# graphic
+# cumulative
+
+df[['R_hodl','R_strat','R_fee','R_net']] = df[['r_hodl','r_strat','r_fee','r_net']].cumprod()
+
+# graphs
+
 from bokeh.plotting import figure,show
 from bokeh.layouts import column,row
 from bokeh.models import DatetimeTickFormatter
 
-df['date'] = pd.to_datetime(df.time, unit='s')
+df['date'] = pd.to_datetime(df['time'], unit='s')
 xformatter = DatetimeTickFormatter(hours="%H:%M", days="%d/%m", months="%m/%Y", years="%Y")
 
-p1 = figure(height=325, width=900)
+p1 = figure(height=325, width=800)
 p1.xaxis[0].formatter = xformatter
-p1.line(df.date, df.close)
-p1.line(df.date, EMA_20, color='red')
-p1.line(df.date, SMA_200, color='green')
+p1.line(df['date'], df['close'])
 
-p2 = figure(height=325, width=900, x_range=p1.x_range)
+p2 = figure(height=325, width=800, x_range=p1.x_range)
 p2.xaxis[0].formatter = xformatter
-p2.line(df.date, df.close, color='gray')
-p2.triangle(df.date, position_in, color='green', size=7)
-p2.inverted_triangle(df.date, position_out, color='red', size=7)
+p2.line(df['date'], df['close'], color='gray')
+p2.line(df['date'], df['slow'], color='red')
+p2.line(df['date'], df['fast'], color='green')
+p2.triangle(df['date'], df['close'].where((df['position'] == 1) & (df['position'].shift() == 0)), color='green', size=7)
+p2.inverted_triangle(df['date'], df['close'].where((df['position'] == 0) & (df['position'].shift() == 1)), color='red', size=7)
 
-p3 = figure(height=125, width=900, x_range=p1.x_range)
+p3 = figure(height=125, width=800, x_range=p1.x_range)
 p3.xaxis[0].formatter = xformatter
-p3.line(df.date, position)
+p3.line(df['date'], df['position'])
 
-p4 = figure(height=150, width=900, x_range=p1.x_range)
+p4 = figure(height=150, width=800, x_range=p1.x_range)
 p4.xaxis[0].formatter = xformatter
-p4.line(df.date, r_0, color='lightgray')
-p4.line(df.date, r_strat)
-p4.line(df.date, r_fee, color='red')
+p4.line(df['date'], df['r_hodl'], color='lightgray')
+p4.line(df['date'], df['r_strat'])
+p4.line(df['date'], df['r_fee'], color='red')
 
-p5 = figure(height=325, width=900, x_range=p1.x_range)
+p5 = figure(height=325, width=800, x_range=p1.x_range)
 p5.xaxis[0].formatter = xformatter
-p5.line(df.date, R_0, color='lightgray')
-p5.line(df.date, R_strat)
-p5.line(df.date, R_net, color='red')
+p5.line(df['date'], df['R_hodl'], color='lightgray')
+p5.line(df['date'], df['R_strat'])
+p5.line(df['date'], df['R_net'], color='red')
 
 layout = column(p1, p2, p3, p4, p5)
 show(layout)
